244669708b52fca91d9a1daabd0ea480cb82abbe
bluehavana <bluehavana@users.noreply.github.com>
2015-02-02 10:27:14 -0800

    install: actual switches. can list individual files. added diff option.

--- install.sh
+++ install.sh
@@ -1,17 +1,93 @@
 #!/bin/bash
 
-set -u
-set -e
+VERSION="0.0"
+
+# don't allow unset vars to be used
+shopt -q -o nounset
+# exit with uncaught command returned error
+shopt -q -o errexit
+# don't overwrite files via redirect
+shopt -q -o noclobber
+# extended glob patterns
 shopt -s extglob
+# globs returning empty are actually empty and not glob pattern
+shopt -s nullglob
+# no hidden files in path expansion
+shopt -u dotglob
+
+usage() {
+
+cat <<EOHELP
+Usage: ${0} [-Dhpv] [-d <dest_dir>] [-s <source_dir>] [ files ]...
+if 'files' are given, the files relative to the source_dir/home
+will be the only files used in the actions
+
+  -d <dest_dir>   use dest_dir instead of ~/
+  -D              Diff all files with current files in ~/
+  -h              Print this help message
+  -p              Just playin', don't actually do anything
+  -s <source_dir> use source_dir instead of copydots
+  -v              version
+EOHELP
+
+}
+
+DIFF_ONLY=false
+declare -a SPECIFIC_FILES
+DEST_DIR=""
+SOURCE_DIR=""
+PLAYIN=false
+
+while getopts "d:Dhps:v" arg
+do
+    case "${arg}" in
+        "d")
+            DEST_DIR="${OPTARG}"
+            ;;
+        "D")
+            DIFF_ONLY=true
+            ;;
+        "h")
+            usage
+            exit 0
+            ;;
+        "p")
+            PLAYIN=true
+            ;;
+        "s")
+            SOURCE_DIR="${OPTARG}"
+            ;;
+        "v")
+            echo "${VERSION}"
+            exit 0
+            ;;
+        \?)
+            usage "${0}" >&2
+            exit 1
+            ;;
+    esac
+
+done
+
+shift $((OPTIND - 1))
 
 # Copy files to DEST_DIR
-DEST_DIR="${1:-$HOME}"
+DEST_DIR="${DEST_DIR:-"${HOME}"}"
 # Copy files from SOURCE_DIR
-SOURCE_DIR="$(dirname "${0}")"
-SOURCE_DIR="${2:-$SOURCE_DIR}"
+SOURCE_DIR="${SOURCE_DIR:-$(dirname "${0}")/copydots}/"
 
-# Just playin': don't actually do anything
-PLAYIN="${PLAYIN:-false}"
+for a in "${@}"
+do
+    if [[ "${a}" ]]
+    then
+        file="${a#./copydots/}"
+        file="${a#copydots/}"
+        if [[ "${file}" ]]
+        then
+            SPECIFIC_FILES=("${SPECIFIC_FILES[@]}" "${file}")
+        fi
+    fi
+done
 
 # Paths that only need a top level message
 SPECIAL_FILENAMES=('config/' 'vim/bundle/')
@@ -109,7 +185,7 @@ fixup_directory() {
     then
         if "${PLAYIN}"
         then
-            format_status playin: mkdir "${dest_dir}"
+            format_status "playin: mkdir $(printf %q ${dest_dir})"
         else
             mkdir "${dest_dir}"
         fi
@@ -140,7 +216,7 @@ copy_file() {
         local status=0
         if "${PLAYIN}"
         then
-            format_status playin: cp "${src}" "${dest}"
+            format_status "playin: cp $(printf %q "${src}") $(printf %q "${dest}")"
             return 0
         else
             cp "${src}" "${dest}"
@@ -164,12 +240,13 @@ copy_file() {
 #
 # returns: 0 if change occured, 1 if unchanged
 sync_files() {
-    local source_dir="$1"
-    local dest_dir="$2"
+    local dest_dir="$1"
+    shift
+    local file_list=("${@}")
 
     local updated=false
 
-    for f in "${source_dir}"/*
+    for f in "${file_list[@]}"
     do
         local clean_f="$(clean_filename "${f}")"
 
@@ -177,7 +254,7 @@ sync_files() {
         then
             if fixup_directory "${dest_dir}/.${clean_f}"
             then
-                if sync_files "${f}" "${dest_dir}" && ! "${updated}"
+                if sync_files "${dest_dir}" "${f}"/* && ! "${updated}"
                 then
                     updated=true
                 fi
@@ -201,10 +278,39 @@ sync_files() {
     ! "${updated}" && return 1
 }
 
-pushd "${SOURCE_DIR}" > /dev/null
-pushd "copydots" > /dev/null
 
-sync_files "./" "${DEST_DIR}" || format_error "Error syncing dotfiles!"
+pushd "${SOURCE_DIR}" > /dev/null
+
+    declare -a file_list
+
+    if [[ "${#SPECIFIC_FILES[@]}" -gt 0 ]]
+    then
+        file_list="${SPECIFIC_FILES[@]}"
+    else
+        file_list=(./*)
+    fi
+
+    if "${DIFF_ONLY}"
+    then
+        errors=0
+
+        for f in "${file_list[@]}"
+        do
+            colordiff "${HOME}/.${f}" "${f}"
+            errors="$?"
+        done
+
+        popd > /dev/null # copydots
+        exit "${errors}"
+    else
+        if ! sync_files "${DEST_DIR}" "${file_list}"
+        then
+            format_error "Error syncing dotfiles!"
+            popd > /dev/null # copydots
+            exit 1
+        fi
+    fi
 
 popd > /dev/null # copydots
-popd > /dev/null # dirname $0
+
+exit 0
